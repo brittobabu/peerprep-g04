dad790153eaab0d85745f74345d2754e8791c5c0
BrittoBabu <brittobabu2000@gmail.com>
2025-03-01 23:06:03 +0800

    added Login/signup

---
+++ frontend/src/app/page.js
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (<>
+    <div>
+      <a href="/auth/login" className="text-blue-500"> LoginPage</a>
+      <a href="/auth/signup" className="text-blue-500">Sign Up</a>
+    </div>
+
+  </>
+  );
+}
+
